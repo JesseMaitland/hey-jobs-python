1292cd2ea8ace30152a6cc8c0c1983630628001e
jesse_maitland <jesse.maitland@zoho.eu>
2018-12-22 12:11:24 +0100

    readme and test comments

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 Project created to submit for the application of Python Developer at Hey Jobs Berlin.
 
-#### Goal
+### Goal
 
 Create a simple web scraper to scrape job adds from Berlin from the first page of HeyJobs.com and save them to a database.
 
 Docker containers for postgres and python provided by Hey Jobs.
 
-#### Running Project
+### Running Project
 
 The project is built using the provided docker containers. Running the following docker commands will start up the containers in the correct order, 
 and copy / run the project automatically in the provided scraper container. The data will then be available
@@ -18,8 +18,10 @@ to select from the database "heyjobs"
 
 `docker-compose up scraper`
 
+### Tests
+tests can be run from the terminal
 
-#### Assumptions
+### Assumptions
 
 - A job add is always wrapped in an `<a></a>` tag.
 - A job uid always has 36 characters
@@ -28,5 +30,46 @@ to select from the database "heyjobs"
 - Database tables can be dropped an rebuilt each time the scraper is run
 
 
-#### 
- 
+### Libraries Used
+
+#### Beautiful Soup 4
+
+**Pros**
+
+- well supported 3rd party library for parsing HTML
+- parsing html can get messy with home rolled solution
+- uses the python html parser
+- solves the parsing problem quite nicely
+
+**Cons**
+
+- not part of python standard library
+- a bit slow
+- scraping / request must be made with another library
+
+#### SQL Alchemy
+
+**Pros**
+
+- well supported most used python orm
+- makes table creation / deletion easy
+- handles simple crud very well, removing the need for messy SQL crud strings in the project
+- raw SQL / stored procedures can still be executed if needed
+- makes project less dependant on specific brand / flavour of SQL
+
+**Cons**
+- not part of python standard library
+- must use another library such as alembic to manage migrations
+- can create more work if DB tables are edited directly and not through the Alchemy Models.
+
+
+### Programming Method - Why use a state machine?
+
+while this scraper could have easily been implemented in a single script with fewer lines of code, fewer files and no directory structure
+a state machine allows program flow to be easily understood, and later modified by simply adding / removing or modifying 
+state objects and transition rules. 
+
+#### Testing
+unit tests can be written to test the function of each individual state, and the expected
+output when passed good or bad data as it's input parameters. This scenario allows for testing
+that the state machine advances to the correct / desired states for a known input
--- heyjobslib/tests.py
+++ heyjobslib/tests.py
@@ -1,28 +1,54 @@
-import unittest
-from .machine import RequestPage, ParsePage, Error, SaveData
+###################################################################################
+#
+#   file for unit tests. Test classes test how each state functions.
+#
+###################################################################################
 
+import unittest
+import logging
+from .machine import RequestPage, ParsePage, Error, SaveData, ExitProgram
+
+
+# disable logging while running unit tests to declutter test output
+logging.disable(logging.CRITICAL)
 
 
 class TestRequestPage(unittest.TestCase):
+
+    """
+    class tests the RequestPage State
+    """
 
     def setUp(self):
         self.good_url = 'http://www.google.com'
         self.bad_url = 'www.google.com'
         self.url_404 = 'http://www.google.com/boo'
 
-    def test_success(self):
+    def test_success_pagerequest(self):
+        """
+        makes a get request to a good url. should return a ParsePage state
+        :return:
+        """
         r = RequestPage(self.good_url)
         r.run()
         self.assertTrue(r.success)
         self.assertTrue(isinstance(r.next(), ParsePage))
 
-    def test_bad_url(self):
+    def test_bad_url_pagerequest(self):
+        """
+        bad url should return an Error state
+        :return:
+        """
         r = RequestPage(self.bad_url)
         r.run()
         self.assertFalse(r.success)
         self.assertTrue(isinstance(r.next(), Error))
 
-    def test_404_url(self):
+    def test_404_url_pagerequest(self):
+        """
+        404 not found should return an Error state
+        :return:
+        """
         r = RequestPage(self.url_404)
         r.run()
         self.assertFalse(r.success)
@@ -30,6 +56,10 @@ class TestRequestPage(unittest.TestCase):
 
 
 class TestParsePage(unittest.TestCase):
+
+    """
+        class tests ParsePage state
+    """
 
     def setUp(self):
         self.html = """
@@ -43,8 +73,14 @@ class TestParsePage(unittest.TestCase):
         </div>
         """
         self.garbage = "akjhdsflkajhdfkja*$(hdlkfjhadslkfj!(#*$)_@hadlkfjhaf,mand.nlchaipher8ydpcia61nerlkthqrhEF"
+        self.bad_html = "<div><div></div></div>"
 
-    def test_success(self):
+    def test_success_parse(self):
+        """
+        tests successful parsing of job add html should create a proper DB JobAdd model
+        and also return a SaveData state
+        :return:
+        """
         p = ParsePage(self.html)
         p.run()
         self.assertEqual(p.db_job_adds[0].title, 'A really great job!')
@@ -52,11 +88,27 @@ class TestParsePage(unittest.TestCase):
         self.assertTrue(p.success)
         self.assertTrue(isinstance(p.next(), SaveData))
         self.assertEqual(len(p.db_job_adds), 2)
+        self.assertEqual(len(p.db_job_adds[0].uid), 36)
 
-    def test_failure(self):
+    def test_failure_parse(self):
+        """
+        passing invalid html should return an error state
+        :return:
+        """
         p = ParsePage(self.garbage)
         p.run()
         self.assertFalse(p.success)
         self.assertTrue(isinstance(p.next(), Error))
 
+    def test_no_jobs_parse(self):
+        """
+        passing good html with no jobs should return success, but not ok to save.
+        should return an exit program state
+        :return:
+        """
+        p = ParsePage(self.bad_html)
+        p.run()
+        self.assertTrue(p.success)
+        self.assertFalse(p.can_save)
+        self.assertTrue(p.next(), ExitProgram)
 
